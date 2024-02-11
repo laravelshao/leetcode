ccefc6aec45740745f7507da1ebfcd5c1c18ae3b
laravelshao <laravelshao@163.com>
2024-02-11 16:49:12 +0800

    add solution #39 Combination Sum change dfs implementation

--- solution/0001-0100/0039.Combination Sum/Solution.java
+++ solution/0001-0100/0039.Combination Sum/Solution.java
@@ -1,8 +1,11 @@
+/**
+ * 参考回溯算法+剪枝(回溯经典例题详解) https://leetcode.cn/problems/combination-sum/solutions/14697/hui-su-suan-fa-jian-zhi-python-dai-ma-java-dai-m-2
+ */
 class Solution {
     // 结果
     private List<List<Integer>> ans = new ArrayList<>();
-    // 临时组合
-    private List<Integer> tmpCombine = new ArrayList<>();
+    // 临时路径组合
+    private List<Integer> path = new ArrayList<>();
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         Arrays.sort(candidates);
@@ -10,20 +13,29 @@ class Solution {
         return ans;
     }
 
+    /**
+     * candidates 候选数组
+     * index 起点下标
+     * target 目标值(每减去一个元素，目标值变小)
+     */
     private void dfs(int[] candidates, int index, int target) {
+
+        // 由于进入更深层时，小于 0 的部分被剪枝，因此递归终止条件值只判断等于 0 的情况
         if (target == 0) {
-            ans.add(new ArrayList(tmpCombine));
+            ans.add(new ArrayList(path));
             return;
         }
 
-        if (index >= candidates.length || candidates[index] > target) {
-            return;
+        for (int i = index; i < candidates.length; i++) {
+            // 剪枝，前提是候选数组已经升序排列
+            if (target < candidates[i]) {
+                break;
+            }
+            path.add(candidates[i]);
+            // 注意：由于每一个元素可以重复使用，下一轮搜索的起点依然是 i，这里非常容易弄错
+            dfs(candidates, i, target - candidates[i]);
+            // 移除最后元素状态重置
+            path.remove(path.size() - 1);
         }
-
-        dfs(candidates, index + 1, target);
-        tmpCombine.add(candidates[index]);
-        dfs(candidates, index, target - candidates[index]);
-        // 移除最后一个元素
-        tmpCombine.remove(tmpCombine.size() - 1);
     }
 }
--- src/java/solution/P0039CombinationSum.java
+++ src/java/solution/P0039CombinationSum.java
@@ -15,8 +15,8 @@ public class P0039CombinationSum {
 
     // 结果
     private static List<List<Integer>> ans = new ArrayList<>();
-    // 临时组合
-    private static List<Integer> tmpCombine = new ArrayList<>();
+    // 临时路径组合
+    private static List<Integer> path = new ArrayList<>();
 
     public static List<List<Integer>> combinationSum(int[] candidates, int target) {
         Arrays.sort(candidates);
@@ -24,23 +24,36 @@ public class P0039CombinationSum {
         return ans;
     }
 
+    /**
+     * candidates 候选数组
+     * index 起点下标
+     * target 目标值(每减去一个元素，目标值变小)
+     */
     private static void dfs(int[] candidates, int index, int target) {
+
+        // 由于进入更深层时，小于 0 的部分被剪枝，因此递归终止条件值只判断等于 0 的情况
         if (target == 0) {
-            ans.add(new ArrayList(tmpCombine));
+            ans.add(new ArrayList(path));
             return;
         }
 
-        if (index >= candidates.length || candidates[index] > target) {
-            return;
+        for (int i = index; i < candidates.length; i++) {
+            // 剪枝，前提是候选数组已经升序排列
+            if (target < candidates[i]) {
+                break;
+            }
+            path.add(candidates[i]);
+            System.out.println("递归之前 => " + path + "，剩余 = " + (target - candidates[i]));
+            // 注意：由于每一个元素可以重复使用，下一轮搜索的起点依然是 i，这里非常容易弄错
+            dfs(candidates, i, target - candidates[i]);
+            // 移除最后元素状态重置
+            path.remove(path.size() - 1);
+            System.out.println("递归之后 => " + path);
         }
-
-        dfs(candidates, index + 1, target);
-        tmpCombine.add(candidates[index]);
-        dfs(candidates, index, target - candidates[index]);
-        tmpCombine.remove(tmpCombine.size() - 1);
     }
 
     public static void main(String[] args) {
-        combinationSum(new int[]{2, 3, 6, 7}, 7);
+        List<List<Integer>> ans = combinationSum(new int[]{2, 3, 6, 7}, 7);
+        System.out.println("输出 => " + ans);
     }
 }
