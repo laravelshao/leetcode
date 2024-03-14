c879e7de758ffb1f631c42b19512e9bebda791c9
laravelshao <laravelshao@163.com>
2024-03-14 17:43:14 +0800

    add solution #237 Delete Node in a Linked List

---
+++ solution/0201-0300/0237.Delete Node in a Linked List/Solution.java
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        // 因为访问不到头节点，可以考虑将node的下一个节点值往前复制
+        node.val = node.next.val;
+        node.next = node.next.next;
+        
+    }
+}
