ef08053b96f952152483eb5afb90e31483df92c7
laravelshao <laravelshao@163.com>
2024-03-17 13:19:15 +0800

    fix #387 First Unique Character in a String

--- solution/0301-0400/0387.First Unique Character in a String/Solution.java
+++ solution/0301-0400/0387.First Unique Character in a String/Solution.java
@@ -1,14 +1,15 @@
 class Solution {
     public int firstUniqChar(String s) {
-        Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> charCntMap = new HashMap<>();
+        // 统计字符次数
         for (int i = 0; i < s.length(); i++) {
-            Character c = s.charAt(i);
-            map.put(c, map.getOrDefault(c, 0) + 1);
+            char c = s.charAt(i);
+            charCntMap.put(c, charCntMap.getOrDefault(c, 0) + 1);
         }
-
+        // 顺序遍历判断字符次数是否等于1
         for (int i = 0; i < s.length(); i++) {
-            Character c = s.charAt(i);
-            if (map.get(c) == 1) {
+            char c = s.charAt(i);
+            if (charCntMap.get(c) == 1) {
                 return i;
             }
         }
