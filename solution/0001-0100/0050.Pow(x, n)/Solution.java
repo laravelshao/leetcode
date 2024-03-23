bf05b790775e9afcf1ffb970854d5433b0683a14
laravelshao <laravelshao@163.com>
2024-03-23 11:44:14 +0800

    fix 50. Pow(x, n)

--- solution/0001-0100/0050.Pow(x, n)/Solution.java
+++ solution/0001-0100/0050.Pow(x, n)/Solution.java
@@ -1,16 +1,17 @@
 class Solution {
     public double myPow(double x, int n) {
         // 如果n是-2^31，-n则会超过int的最大边界，利用long接收
-        long N = n;
-        return N > 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);
+        long l = n;
+        return l > 0 ? quickMul(x, l) : 1 / quickMul(x, -l);
     }
-    private double quickMul(double x, long N) {
-        if (N == 0) {
+
+    private double quickMul(double x, long n) {
+        if (n == 0) {
             return 1.0;
         }
 
-        double y = quickMul(x, N / 2);
+        double y = quickMul(x, n / 2);
         // 如果N%2不等于0，需要额外乘以x
-        return N % 2 == 0 ? y * y : y * y * x;
+        return n % 2 == 0 ? y * y : y * y * x;
     }
 }
