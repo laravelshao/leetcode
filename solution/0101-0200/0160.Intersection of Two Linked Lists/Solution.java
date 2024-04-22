b8c76260b60f830a2598f7f582e06de3ab976fdc
laravelshao <laravelshao@163.com>
2024-04-22 22:59:59 +0800

    fix 160.Intersection of Two Linked Lists

--- solution/0101-0200/0160.Intersection of Two Linked Lists/Solution.java
+++ solution/0101-0200/0160.Intersection of Two Linked Lists/Solution.java
@@ -14,8 +14,8 @@ public class Solution {
         ListNode A = headA, B = headB;
         while (A != B) {
             // 假设A链表节点数为a，B链表节点数为b，公共尾部节点数为c
-            // 先遍历A链表再遍历B链表到橡胶节点时，步数为 a + b - c
-            // 先遍历B链表再遍历A链表到橡胶节点时，步数为 b + a - c
+            // 先遍历A链表再遍历B链表到相交节点时，步数为 a + b - c
+            // 先遍历B链表再遍历A链表到相交节点时，步数为 b + a - c
             // 如果有相交，A、B都指向相交节点；否则都指向null
             A = A != null ? A.next : headB;
             B = B != null ? B.next : headA;
