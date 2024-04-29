1d11776d83893f4b24be53205a15ef8272acf848
laravelshao <laravelshao@163.com>
2024-04-29 15:13:29 +0800

    fix solution 139. Word Break

--- solution/0101-0200/0139.Word Break/Solution.java
+++ solution/0101-0200/0139.Word Break/Solution.java
@@ -3,13 +3,12 @@ class Solution {
         // 状态定义：dp[i] 为字符串前i个字符串[0, i - 1]是否可以由 wordDict 中的字符串组成
         // 转移方程：dp[i] = dp[j] && wordDict是否包含子字符串s[j, i - 1]
         // 初始状态：dp[0] = true，代表空字符串
-        HashSet<String> wordDictSet = new HashSet<>(wordDict);
         boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true; // 初始状态
         for (int i = 1; i <= s.length(); i++) {
             for (int j = 0; j < i; j++) {
                 // substring：截取包括左下标不包含右下标[j, i - 1]
-                if (dp[j] && wordDictSet.contains(s.substring(j, i))) {
+                if (dp[j] && wordDict.contains(s.substring(j, i))) {
                     dp[i] = true;
                     break;
                 }
