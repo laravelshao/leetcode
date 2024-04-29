3b4d71331eae2904d03be3f4c0b8676ee833ef5c
laravelshao <laravelshao@163.com>
2024-04-29 11:55:21 +0800

    fix solution 200. Number of Islands

--- solution/0101-0200/0200.Number of Islands/Solution.java
+++ solution/0101-0200/0200.Number of Islands/Solution.java
@@ -3,6 +3,7 @@ class Solution {
         int ans = 0;
         for(int i = 0; i < grid.length; i++) {
             for(int j = 0; j < grid[0].length; j++) {
+                // 当节点是陆地'1'时开始dfs
                 if(grid[i][j] == '1') {
                     dfs(grid, i, j);
                     ans++;
@@ -14,11 +15,13 @@ class Solution {
     }
 
     private void dfs(char[][] grid, int i, int j) {
+        // 上下左右查询可能会越界
         if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0') {
             return;
         }
-
+        // 遍历完之后将其置为'0'，防止后续再被遍历到
         grid[i][j] = '0';
+        // 针对当前位置的上下左右进行进行深度遍历
         dfs(grid, i - 1, j);
         dfs(grid, i + 1, j);
         dfs(grid, i, j - 1);
