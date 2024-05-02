775c2fafd31c8e3e2dd5d5b282a19626879a83b7
laravelshao <laravelshao@163.com>
2024-05-02 15:40:05 +0800

    fix 72. Edit Distance

--- solution/0001-0100/0072.Edit Distance/Solution.java
+++ solution/0001-0100/0072.Edit Distance/Solution.java
@@ -1,36 +1,41 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        // 状态定义：dp[i, j] 表示将word1的前i个字符更改为word2的前j个字符所需的最少编辑步数
-        // 针对子问题dp[i, j]，其对应的2个字符串尾部字符word1[i - 1] 和 word2[j - 1]
-        // 1、在word1[i - 1]后添加word2[j - 1]，则剩余子问题为 dp[i, j - 1]
-        // 2、删除word1[i - 1]，则剩余子问题为 dp[i - 1][j]
-        // 3、将word1[i - 1]替换为word2[j - 1]，剩余子问题为 dp[i - 1][j - 1]
-        // 转移方程：dp[i, j] = min(dp[i, j - 1], dp[i - 1, j], dp[i - 1, j - 1]) + 1
-        // 字符word1[i - 1] 和 word2[j - 1] 一致时，dp[i, j] = dp[i - 1, j - 1]
+        // 状态定义：dp[i, j] 代表将 word1 的前 i 个字符转换为 word2 的前 j 个字符的最少操作步数
+
+        // 针对替换、删除、插入操作说明：
+        // 1、替换操作：word1 的前 i-1 个字符已经变换到 word2 的前 j-1 个字符的次数，说明 word1 的前 i-1 个和 word2 的前 j-1 个字符已经完成操作；
+        // 那么对于 word1 的第 i 个怎么变成 word2 的第 j 个呢？这两个字符都存在，那么只能是替换了；所以 dp[i][j] = dp[i-1][j-1] + 1;
+        // 2、插入操作：word1 的前 i 个字符已经变换到 word2 的前 j-1 个字符的次数，当前 word1 的第 i 步字符已经用了，
+        // 但是 word2 还差一个字符（因为当前只是处理了 word2 的前 j-1 个字符），那么插入一个字符就好了；所以 dp[i][j] = dp[i][j-1] + 1;
+        // 3、删除操作：word1 的前 i-1 个字符已经变换到 word2 的前 j 个字符的次数，当前 word1 仅用了前 i-1 个字符就完成了到 word2 的前 j 个字符的操作，
+        // 所以 word1 的第 i 个字符其实没啥用了，那么删除操作就好了；所以 dp[i][j] = dp[i-1][j] + 1;
+        // 转移方程：
+        // 1、word1[i] != word2[j]，dp[i, j] = min(dp[i-1][j-1], dp[i][j-1], dp[i-1][j]) + 1
+        // 2、word1[i] == word2[j]，dp[i, j] = dp[i - 1, j - 1]
+
         // 初始状态：
         // 1、当两个字符串都为空时，dp[0][0] = 0
-        // 2、当word1字符串为空时，最少编辑步数等于word2的长度，首行为空，dp[0][j] = j
-        // 3、当word2字符串为空时，最少编辑步数等于word1的长度，首列为空，dp[i][0] = i
+        // 2、当 word1 字符串为空时，则最少编辑步数等于 word2 的长度，首行为空，dp[0][j] = j
+        // 3、当 word2 字符串为空时，则最少编辑步数等于 word1 的长度，首列为空，dp[i][0] = i
 
         int m = word1.length(), n = word2.length();
         int[][] dp = new int[m + 1][n + 1];
 
-        // 填充首列初始状态
-        for (int i = 1; i <= m; i++) {
-            dp[i][0] = i;
+        // 初始化首行
+        for(int i = 1; i <= n; i++) {
+            dp[0][i] = i;
+        }
+        // 初始化首列
+        for(int j = 1; j <= m; j++) {
+            dp[j][0] = j;
         }
 
-        // 填充首行初始状态
-        for (int j = 1; j <= n; j++) {
-            dp[0][j] = j;
-        }
-
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(word1.charAt(i - 1) == word2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1];
                 } else {
-                    dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
                 }
             }
         }
