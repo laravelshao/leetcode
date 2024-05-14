0135562d5d201feeb3cebe98031cf8faedbca199
laravelshao <laravelshao@163.com>
2024-05-14 17:21:15 +0800

    fix 19. Remove Nth Node From End of List

--- solution/0001-0100/0019.Remove Nth Node From End of List/Solution.java
+++ solution/0001-0100/0019.Remove Nth Node From End of List/Solution.java
@@ -10,21 +10,25 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        // 添加哑节点
+
         ListNode dummy = new ListNode(0, head);
-        // 双指针法，需要找到倒数第N个元素
-        ListNode first = dummy, second = dummy;
-        // first先遍历n次
-        while(n-- > 0) {
-            first = first.next;
+        // 双指针法，需要找到倒数第 n 个元素
+        ListNode slow = dummy, fast = dummy;
+        // 快指针先遍历 n 次
+        while (n-- > 0) {
+            fast = fast.next;
         }
-        // first和second一起遍历，first遍历到最后，此时second刚好和first相差n，刚好是倒数第N个元素的前一位置
-        while(first.next != null) {
-            first = first.next;
-            second = second.next;
+
+        // 快慢指针一起走，当快指针到最后时，慢指针刚好到倒数第 n 个节点的前面，快慢指针相差 n
+        // 快慢指针一起遍历，快指针遍历到最后，此时慢指针刚好和快指针相差 n，刚好是倒数第 n 个元素的前一位置
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
         }
-        // 直接将倒数第N个元素的前一位置的元素指向下下节点
-        second.next = second.next.next;
+
+        // 直接将倒数第 n 个元素的前一位置的元素指向下下节点
+        slow.next = slow.next.next;
+
         return dummy.next;
     }
 }
