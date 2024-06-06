9d6de8e7e9aabb367eec38c16fdc9b853b75a3bb
laravelshao <laravelshao@163.com>
2024-06-06 21:16:47 +0800

    fix 322. Coin Change

--- solution/0301-0400/0322.Coin Change/Solution.java
+++ solution/0301-0400/0322.Coin Change/Solution.java
@@ -1,7 +1,10 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         // 状态定义：dp[i] = 表示凑足金额i的最少硬币数
-        // 转移方程：dp[i] = min(dp[i], dp[i- coins[j]] + 1)
+        // 转移方程：dp[i] = min(dp[i], dp[i- coins[j]] + 1)，j ∈ [0, coins.length - 1]
+        // 包含2种情况：
+        // 1、不选当前硬币，为 dp[i]
+        // 2、选择当前硬币，剩余金额为i-coins[j]，硬币数为 dp[i - coins[j]] + 1
         // 初始状态：dp[0] = 0
         int max = amount + 1;
         int[] dp = new int[amount + 1];
@@ -12,7 +15,7 @@ class Solution {
                 if(coins[j] <= i) {
                     // 包含2种情况：
                     // 1、不选当前硬币，为 dp[i]
-                    // 2、选择当前硬币，剩余金额为i-coins[j]，硬币数为dp[i - coins[j]] + 1
+                    // 2、选择当前硬币，剩余金额为i-coins[j]，硬币数为 dp[i - coins[j]] + 1
                     dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
                 }
             }
