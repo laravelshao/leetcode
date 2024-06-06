f7a9e1075ed4f9d2d4e28f5ecdd0c735e7324708
laravelshao <laravelshao@163.com>
2024-06-06 21:04:46 +0800

    fix 139. Word Break

--- solution/0101-0200/0139.Word Break/Solution.java
+++ solution/0101-0200/0139.Word Break/Solution.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         // 状态定义：dp[i] 为字符串前i个字符串[0, i - 1]是否可以由 wordDict 中的字符串组成
-        // 转移方程：dp[i] = dp[j] && wordDict是否包含子字符串s[j, i - 1]
+        // 转移方程：dp[i] = dp[j] && wordDict是否包含子字符串s[j, i - 1]，j ∈ [0, i)
         // 初始状态：dp[0] = true，代表空字符串
         boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true; // 初始状态
