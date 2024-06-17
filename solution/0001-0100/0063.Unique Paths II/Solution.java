3ca517fd33981ea858796238e3fd140011b0e48e
laravelshao <laravelshao@163.com>
2024-06-17 20:37:49 +0800

    fix 63. Unique Paths II

--- solution/0001-0100/0063.Unique Paths II/Solution.java
+++ solution/0001-0100/0063.Unique Paths II/Solution.java
@@ -2,19 +2,18 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
 
         // 状态定义：dp[i, j] 为指定索引下标的路径数，只能由左或上移动
-        // 转移方程：dp[i, j] = dp[i, j - 1] + dp[i - 1, j] 
+        // 转移方程：dp[i, j] = dp[i, j - 1] + dp[i - 1, j]
         // 初始状态：第一行第一列，碰到障碍物(1)后停止 dp[0, j] = 1，dp[i, 0] = 1
         int m = obstacleGrid.length, n = obstacleGrid[0].length;
         int[][] dp = new int[m][n];
 
-        // 填充第一列初始值，碰到障碍物后停止
-        for (int i = 0; i < m && obstacleGrid[i][0] == 0; i++) {
-            dp[i][0] = 1;
-        }
-
         // 填充第一行初始值，碰到障碍物后停止
         for (int j = 0; j < n && obstacleGrid[0][j] == 0; j++) {
             dp[0][j] = 1;
+        }
+        // 填充第一列初始值，碰到障碍物后停止
+        for (int i = 0; i < m && obstacleGrid[i][0] == 0; i++) {
+            dp[i][0] = 1;
         }
 
         for (int i = 1; i < m; i++) {
