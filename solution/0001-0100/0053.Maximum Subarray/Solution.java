542c2009dc39961d2ab8e30b3100a595dc2c5e31
laravelshao <laravelshao@163.com>
2024-06-24 21:06:15 +0800

    fix 53. Maximum Subarray

--- solution/0001-0100/0053.Maximum Subarray/Solution.java
+++ solution/0001-0100/0053.Maximum Subarray/Solution.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        // 状态定义：dp[i] 代表以 nums[i] 作为结尾最大和连续子数组
+        // 状态定义：dp[i] 代表以 nums[i] 作为结尾连续子数组最大和
         // 解释：如果前置dp[i-1]是负数，加上nums[i]只会更小，所以从当前元素开始从新统计
         // 转移方程：dp[i] = max(dp[i - 1] + nums[i], nums[i])
         // 初始状态：dp[0] = nums[0];
