d5d163fb713aaf048d6d22f1214b4637280c49d2
laravelshao <laravelshao@163.com>
2024-06-26 20:55:32 +0800

    fix 17. Letter Combinations of a Phone Number

--- solution/0001-0100/0017.Letter Combinations of a Phone Number/Solution.java
+++ solution/0001-0100/0017.Letter Combinations of a Phone Number/Solution.java
@@ -14,32 +14,33 @@ class Solution {
         }
     };
 
+    private List<String> ans = new ArrayList<>();
+
     private StringBuilder str = new StringBuilder();
 
     public List<String> letterCombinations(String digits) {
-        List<String> ans = new ArrayList<>();
         if (digits.length() == 0) {
             return ans;
         }
 
-        dfs(ans, digits, 0);
+        dfs(digits, 0);
         return ans;
     }
 
-    public void dfs(List<String> ans, String digits, int index) {
+    public void dfs(String digits, int index) {
         if (index == digits.length()) {
             ans.add(str.toString());
             return;
-        } else {
-            // 获取指定索引数字对应的字母
-            String letters = numLetterMap.get(digits.charAt(index));
-            for (int i = 0; i < letters.length(); i++) {
-                // 遍历字母追加到之前的组合后
-                str.append(letters.charAt(i));
-                dfs(ans, digits, index + 1);
-                // 删除最后一个
-                str.deleteCharAt(index);
-            }
+        }
+
+        // 获取指定索引数字对应的字母
+        String letters = numLetterMap.get(digits.charAt(index));
+        for (int i = 0; i < letters.length(); i++) {
+            // 遍历字母追加到之前的组合后
+            str.append(letters.charAt(i));
+            dfs(digits, index + 1);
+            // 删除最后一个
+            str.deleteCharAt(index);
         }
     }
 }
