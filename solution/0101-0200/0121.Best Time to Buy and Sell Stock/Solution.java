e0604ad186350db5532f0a15cd7983ba5d15fd1c
laravelshao <laravelshao@163.com>
2024-07-04 20:35:42 +0800

    fix 121. Best Time to Buy and Sell Stock

--- solution/0101-0200/0121.Best Time to Buy and Sell Stock/Solution.java
+++ solution/0101-0200/0121.Best Time to Buy and Sell Stock/Solution.java
@@ -1,9 +1,9 @@
 class Solution {
     public int maxProfit(int[] prices) {
 
-        // 状态转移方程：dp(i) = max(dp(i - 1), price - cost)
-        // dp(i)：代表前 i 天的最大利润，dp(i - 1)：代表前 i - 1 天的最大利润 
-        // price - cost：第 i 天卖出的最高利润
+        // 状态定义：dp(i) 代表前 i 天的最大利润，dp(i - 1) 代表前 i - 1 天的最大利润
+        // 转移方程：dp(i) = max(dp(i - 1), price - cost)，price - cost：代表第 i 天卖出的最高利润
+        // 初始状态：dp(0) = 0
         int cost = Integer.MAX_VALUE, profit = 0;
         for (int price : prices) {
             cost = Math.min(cost, price);
