b56e3ff3325d63892e8d9ef965e4ffa13471a95e
laravelshao <laravelshao@163.com>
2024-07-08 20:05:18 +0800

    fix 122. Best Time to Buy and Sell Stock II

--- solution/0101-0200/0122.Best Time to Buy and Sell Stock II/Solution.java
+++ solution/0101-0200/0122.Best Time to Buy and Sell Stock II/Solution.java
@@ -2,10 +2,10 @@ class Solution {
     public int maxProfit(int[] prices) {
         int profit = 0;
         for (int i = 1; i < prices.length; i++) {
-            // 等价于每天都买卖
-            int tmpProfit = prices[i] - prices[i - 1];
-            if (tmpProfit > 0) {
-                profit += tmpProfit;
+            // 执行贪心策略，等价于每天都买卖，计算当天与前一天的利润，大于0则累加到总利润
+            int tmp = prices[i] - prices[i - 1];
+            if (tmp > 0) {
+                profit += tmp;
             }
         }
 
