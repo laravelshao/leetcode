cc9ae1b49d3e670374b6ee4e58a314708efde901
laravelshao <laravelshao@163.com>
2024-07-08 20:34:07 +0800

    fix 55. Jump Game

--- solution/0001-0100/0055.Jump Game/Solution.java
+++ solution/0001-0100/0055.Jump Game/Solution.java
@@ -1,15 +1,13 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        // 状态定义：dp(i) 代表下标i能够到达的最远下标
-        // 转移方程：dp(i) = max(dp(i - 1), i + nums[i]);
-        // 初始状态：dp(0) = 0
-        int dp = 0;
-        for(int i = 0; i < nums.length; i++) {
+        // max：代表能够到达的最远下标
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
             // 最远下标小于当前位置说明不可达
-            if(dp < i) {
+            if (max < i) {
                 return false;
             }
-            dp = Math.max(dp, i + nums[i]);
+            max = Math.max(max, i + nums[i]);
         }
         return true;
     }
