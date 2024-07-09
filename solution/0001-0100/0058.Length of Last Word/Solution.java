122380572e5fb84ec929fcdc8566a80c357eac0f
laravelshao <laravelshao@163.com>
2024-07-09 20:57:39 +0800

    fix 58. Length of Last Word

--- solution/0001-0100/0058.Length of Last Word/Solution.java
+++ solution/0001-0100/0058.Length of Last Word/Solution.java
@@ -1,6 +1,6 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        
+
         int index = s.length() - 1;
         // 从末尾开始遍历找到最后一个单词的最后一个字母下标
         while (index >= 0 && s.charAt(index) == ' ') {
@@ -8,6 +8,7 @@ class Solution {
         }
 
         int wordLen = 0;
+        // 继续往前找到第一个空字符的下标，每遍历一次单词长度自增1
         while (index >= 0 && s.charAt(index) != ' ') {
             wordLen++;
             index--;
