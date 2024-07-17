fbdc385c9d78a20c38ea0b13cf402b23be567b81
laravelshao <laravelshao@163.com>
2024-07-17 20:26:52 +0800

    fix 14. Longest Common Prefix

--- solution/0001-0100/0014.Longest Common Prefix/Solution.java
+++ solution/0001-0100/0014.Longest Common Prefix/Solution.java
@@ -1,7 +1,7 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
 
-        // 先确定最短的字符串，因为最小的子串肯定小于等于最短的字符串
+        // 先确定最短的字符串，因为最小子串肯定小于等于最短字符串
         String minStr = strs[0];
         for (String s : strs) {
             if (s.length() < minStr.length()) {
@@ -9,9 +9,10 @@ class Solution {
             }
         }
 
-        // 从最长子串开始进行比较
+        // 遍历该子串，从末尾依次减少一个字符，然后和字符串数组的前缀进行比较
         for (int i = minStr.length(); i >= 0; i--) {
             String prefix = minStr.substring(0, i);
+            // 判断前缀相同的个数，如果个数和字符串数组长度一样，则返回该子串前缀，否则继续减少字符
             int count = 0;
             for (String s : strs) {
                 if (!prefix.equals(s.substring(0, i))) {
