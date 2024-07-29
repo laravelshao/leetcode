80f754a2be539953821a36ad1b4c754eb8245fed
laravelshao <laravelshao@163.com>
2024-07-29 20:09:13 +0800

    fix 209. Minimum Size Subarray Sum

--- solution/0201-0300/0209.Minimum Size Subarray Sum/Solution.java
+++ solution/0201-0300/0209.Minimum Size Subarray Sum/Solution.java
@@ -1,20 +1,26 @@
 class Solution {
-    /**
-     * 双指针方式
-     */
     public int minSubArrayLen(int target, int[] nums) {
-        // length：数组长度 minLength：最小长度
-        int length = nums.length, minLength = nums.length + 1;
-        long sum = 0;
-        for (int i = 0, j = 0; i < length; ++i) {
-            sum += nums[i];
-            while (j < length && sum >= target) {
-                // 当双指针窗口中累加 >= target，则计算最小长度
-                minLength = Math.min(minLength, i - j + 1);
-                // j = 0 从左开始一个一个移除元素
-                sum -= nums[j++];
-            }
+
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
         }
-        return minLength <= length ? minLength : 0;
+
+        int ans = Integer.MAX_VALUE;
+        // start、end 对应滑动窗口的起始结束位置
+        int start = 0, end = 0, sum = 0;
+        while (end < n) {
+            sum += nums[end];
+            // 当双指针窗口中累加 >= target，则计算最小长度
+            while (sum >= target) {
+                ans = Math.min(ans, end - start + 1);
+                // 从左开始一个一个移除元素
+                sum -= nums[start];
+                start++;
+            }
+            end++;
+        }
+
+        return ans == Integer.MAX_VALUE ? 0 : ans;
     }
 }
