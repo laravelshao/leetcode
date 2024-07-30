4525957809b0899807514f2042b22a21eca33f03
laravelshao <laravelshao@163.com>
2024-07-30 20:08:53 +0800

    fix 35. Search Insert Position

--- solution/0001-0100/0035.Search Insert Position/Solution.java
+++ solution/0001-0100/0035.Search Insert Position/Solution.java
@@ -1,18 +1,19 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
 
-        int low = 0, high = nums.length - 1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
+        int left = 0, right = nums.length - 1;
+        // 闭区间方式
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] < target) {
-                low = mid + 1;
-            } else if (nums[mid] > target) {
-                high = mid - 1;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
 
-        return low;
+        return left;
     }
 }
