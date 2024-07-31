a45db2a2be3ca92bf8057820c1b9fc058fb864bc
laravelshao <laravelshao@163.com>
2024-07-31 21:30:43 +0800

    fix 153. Find Minimum in Rotated Sorted Array

--- solution/0101-0200/0153.Find Minimum in Rotated Sorted Array/Solution.java
+++ solution/0101-0200/0153.Find Minimum in Rotated Sorted Array/Solution.java
@@ -1,18 +1,13 @@
 class Solution {
     public int findMin(int[] nums) {
 
-        int len = nums.length;
-        if (nums[0] <= nums[len - 1]) {
-            return nums[0];
-        }
-
-        int left = 0, right = len - 1;
+        int left = 0, right = nums.length - 1;
         while (left < right) {
-            int mid = (left + right) / 2;
-            if (nums[0] <= nums[mid]) {
-                left = mid + 1;
-            } else {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < nums[right]) {
                 right = mid;
+            } else {
+                left = mid + 1;
             }
         }
 
