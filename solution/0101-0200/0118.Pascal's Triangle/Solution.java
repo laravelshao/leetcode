a99ca660967f9c52fbd00e84a21f31bc832d9cf7
laravelshao <laravelshao@163.com>
2024-08-08 13:29:28 +0800

    fix 118. Pascal's Triangle

--- solution/0101-0200/0118.Pascal's Triangle/Solution.java
+++ solution/0101-0200/0118.Pascal's Triangle/Solution.java
@@ -1,24 +1,23 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
 
-        // 结果列表
-        List<List<Integer>> result = new ArrayList<>(numRows);
-        // 填充第一行
-        result.add(Arrays.asList(1));
-        for (int i = 0; i < numRows - 1; i++) {
-            List<Integer> tmpList = new ArrayList<>();
-            // 头部填1
-            tmpList.add(1);
-            // 第2行，j<0，不满足
-            // 第3行，j<1，执行1次循环，添加元素2
-            for (int j = 0; j < result.get(i).size() - 1; j++) {
-                tmpList.add(result.get(i).get(j) + result.get(i).get(j + 1));
+        // 转移方程：dp(i, j) = dp(i - 1, j - 1) + dp(i - 1, j)
+        // 初始状态：每行首尾都是1
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            for(int j = 0; j <= i; j++) {
+                // 每行首尾默认添加1
+                if(j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    // 非行首行尾 dp(i, j) = dp(i - 1, j - 1) + dp(i - 1, j)
+                    row.add(ans.get(i - 1).get(j - 1) + ans.get(i - 1).get(j));
+                }
             }
-            // 尾部填1
-            tmpList.add(1);
-            result.add(tmpList);
+            ans.add(row);
         }
 
-        return result;
+        return ans;
     }
 }
