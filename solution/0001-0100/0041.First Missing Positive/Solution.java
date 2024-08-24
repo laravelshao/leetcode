0b24fa359476dc14de856947ea6096d4f2e409b3
laravelshao <laravelshao@163.com>
2024-08-24 13:15:25 +0800

    fix 41. First Missing Positive

--- solution/0001-0100/0041.First Missing Positive/Solution.java
+++ solution/0001-0100/0041.First Missing Positive/Solution.java
@@ -1,10 +1,9 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-
         int n = nums.length;
-        // 原地哈希，将指定的数 x 交换到下标为 x - 1 的位置
-        // 对于一个长度为 N 的数组，其中没有出现的最小正整数只能在 [1,N+1] 中。这是因为
-        // 如果 [1,N] 都出现了，那么答案是 N+1，否则答案是 [1,N] 中没有出现的最小正整数。
+        // 原地哈希：将指定的数 x 交换到下标为 x - 1 的位置
+        // 没有出现的最小正整数只会在[1, n + 1] 之间
+        // 如果 [1, n] 都出现了，那么答案是 n + 1；否则答案是 [1, n] 中没有出现的最小正整数
         for (int i = 0; i < n; i++) {
             // 在[1, n]之间 且 当前数 nums[i] 未待在该待的 nums[i] - 1 下标处
             while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
@@ -15,6 +14,7 @@ class Solution {
         }
 
         for (int i = 0; i < n; i++) {
+            // 未待在其指定下标位置，缺失的就是最小正整数
             if (nums[i] != i + 1) {
                 return i + 1;
             }
