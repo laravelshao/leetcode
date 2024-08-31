5475f89ae335fb82d3b4e1712e3b72a70a627829
laravelshao <laravelshao@163.com>
2024-08-31 12:29:04 +0800

    fix 240. Search a 2D Matrix II

--- solution/0201-0300/0240.Search a 2D Matrix II/Solution.java
+++ solution/0201-0300/0240.Search a 2D Matrix II/Solution.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        // 将矩阵看为以右上角为根节点的二叉树
+
+        // 将矩阵看做为以右上角为根节点的二叉树，从左下角开始遍历
         int i = matrix.length - 1, j = 0;
         while (i >= 0 && j < matrix[0].length) {
             if (matrix[i][j] > target) {
