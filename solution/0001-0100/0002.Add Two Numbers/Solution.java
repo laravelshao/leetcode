917d96065de528aec384e10a0c8a252a46a25bd4
laravelshao <laravelshao@163.com>
2024-09-01 12:34:21 +0800

    fix 2. Add Two Numbers

--- solution/0001-0100/0002.Add Two Numbers/Solution.java
+++ solution/0001-0100/0002.Add Two Numbers/Solution.java
@@ -10,22 +10,23 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode result = new ListNode(0);
+
+        ListNode ans = new ListNode();
+        ListNode cur = ans;
         // 进位值
         int carry = 0;
-        ListNode current = result;
         while (l1 != null || l2 != null || carry != 0) {
             // 当前位置总和
-            int sum = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carry;
+            int sum = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0) + carry;
             // 进位值等于 sum/10
             carry = sum / 10;
             // 当前位置值 sum%10
-            current.next = new ListNode(sum % 10);
-            current = current.next;
-            l1 = (l1 == null) ? null : l1.next;
-            l2 = (l2 == null) ? null : l2.next;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+            l1 = l1 != null ? l1.next : null;
+            l2 = l2 != null ? l2.next : null;
         }
 
-        return result.next;
+        return ans.next;
     }
 }
