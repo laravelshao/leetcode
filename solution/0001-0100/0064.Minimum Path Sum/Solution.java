a1b034694078453ea055399e41d30d131b55f63e
laravelshao <laravelshao@163.com>
2024-09-07 15:09:09 +0800

    fix 64. Minimum Path Sum

--- solution/0001-0100/0064.Minimum Path Sum/Solution.java
+++ solution/0001-0100/0064.Minimum Path Sum/Solution.java
@@ -1,33 +1,28 @@
 class Solution {
     public int minPathSum(int[][] grid) {
 
-        int rows = grid.length; // 行
-        int columns = grid[0].length; // 列
-        /**
-         * 第一列：i > 0 且 j = 0, dp[i][0] = dp[i-1][0] + grid[i][0]
-         * 第一行：i = 0 且 j > 0, dp[0][j] = dp[0][j-1] + grid[0][j];
-         * 其它：i > 0 且 j > 0, dp[i][j] = min(dp[i][j-1], dp[i-1][j]) + grid[i][j];
-         */
-        int[][] dp = new int[rows][columns];
-        dp[0][0] = grid[0][0];
+        // 状态定义：dp[i, j] 表示下标位置(i, j)的最小路径和
+        // 解释：只能由上面或左边移动
+        // 转移方程：dp[i, j] = min(dp[i - 1, j], dp[i, j - 1]) + grid[i][j]
+        // 初始状态：第一行只能向右移动，第一列只能向下移动
 
-        // 第一列
-        for (int i = 1; i < rows; i++) {
+        int m = grid.length, n = grid[0].length;
+        int[][] dp = new int[m][n];
+        // 初始化首行、首列
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+        for (int i = 1; i < m; i++) {
             dp[i][0] = dp[i - 1][0] + grid[i][0];
         }
 
-        // 第一行
-        for (int j = 1; j < columns; j++) {
-            dp[0][j] = dp[0][j - 1] + grid[0][j];
-        }
-
-        // 其它
-        for (int i = 1; i < rows; i++) {
-            for (int j = 1; j < columns; j++) {
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
                 dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
             }
         }
 
-        return dp[rows - 1][columns - 1];
+        return dp[m - 1][n - 1];
     }
 }
