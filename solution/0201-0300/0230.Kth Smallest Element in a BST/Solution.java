03a64f27b701b45feadf5bd726a1187e072da49e
laravelshao <laravelshao@163.com>
2024-09-09 19:34:52 +0800

    fix 230. Kth Smallest Element in a BST

--- solution/0201-0300/0230.Kth Smallest Element in a BST/Solution.java
+++ solution/0201-0300/0230.Kth Smallest Element in a BST/Solution.java
@@ -25,7 +25,7 @@ class Solution {
 
     private void dfs(TreeNode root) {
         // 二拆搜索树满足条件：左子节点 < 根节点 < 右子节点
-        // 中旬遍历：先遍历根节点，再遍历左子节点，后遍历右子节点
+        // 中旬遍历：先遍历左子节点，再遍历根节点，后遍历右子节点
         if (root == null) {
             return;
         }
