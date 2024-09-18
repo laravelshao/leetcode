d7c8ee17bfc6cbe42826fc12eacb9d58e1ae9047
laravelshao <laravelshao@163.com>
2024-09-18 20:27:53 +0800

    fix 62. Unique Paths

--- solution/0001-0100/0062.Unique Paths/Solution.java
+++ solution/0001-0100/0062.Unique Paths/Solution.java
@@ -1,21 +1,20 @@
 class Solution {
-    /**
-     * 状态转移方程：dp(i,j)=dp(i-1,j)+dp(i,j-1)
-     */
     public int uniquePaths(int m, int n) {
-        int[] f = new int[n];
-
-        // 将第一行元素初始化为1
-        for(int i = 0; i < n; i++) {
-            f[i] = 1;
-        }
+        // 状态定义：dp(i, j) 表示达到下标(i, j)的路径数
+        // 转移方程：dp(i, j) = dp(i - 1, j) + dp(i, j - 1)
+        // 初始状态：首行首列都为1
+        // 因为dp(i) 只和dp(i - 1)相关，可以简化为一维数组
+        // 转移方程：dp(i) = dp(i) + dp(i - 1)
+        int[] dp = new int[n];
+        // 初始化第一行为1
+        Arrays.fill(dp, 1);
 
         for(int i = 1; i < m; i++) {
             for(int j = 1; j < n; j++) {
-                f[j] += f[j - 1];
+                dp[j] += dp[j - 1];
             }
         }
 
-        return f[n - 1];
+        return dp[n - 1];
     }
 }
