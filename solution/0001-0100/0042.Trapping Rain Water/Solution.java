8493b90816cef1e3d4a78ee413241d494135f1ce
laravelshao <laravelshao@163.com>
2024-09-21 14:44:48 +0800

    fix 42. Trapping Rain Water

---
+++ solution/0001-0100/0042.Trapping Rain Water/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int trap(int[] height) {
+        int ans = 0;
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        while (left < right) {
+            leftMax = Math.max(leftMax, height[left]);
+            rightMax = Math.max(rightMax, height[right]);
+            // 如果 height[left] < height[right]，则 leftMax < rightMax，则 ans 加上较小的 leftMax - height[left]
+            if (height[left] < height[right]) {
+                ans += leftMax - height[left];
+                left++;
+            } else {
+                ans += rightMax - height[right];
+                right--;
+            }
+        }
+
+        return ans;
+    }
+}
