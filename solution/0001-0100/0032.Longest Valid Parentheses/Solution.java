8bed6b1ad076ff51073c02d84eb8d2e7057a8bf7
laravelshao <laravelshao@163.com>
2024-10-06 18:03:32 +0800

    fix 32. Longest Valid Parentheses

--- solution/0001-0100/0032.Longest Valid Parentheses/Solution.java
+++ solution/0001-0100/0032.Longest Valid Parentheses/Solution.java
@@ -2,6 +2,7 @@ class Solution {
     public int longestValidParentheses(String s) {
 
         int ans = 0;
+        // 栈存储左括号下标
         Deque<Integer> stack = new LinkedList<Integer>();
         stack.push(-1);
         for (int i = 0; i < s.length(); i++) {
@@ -12,6 +13,7 @@ class Solution {
                 // 右括号出栈
                 stack.pop();
                 if (stack.isEmpty()) {
+                    // 栈为空，说明当前右括号无法匹配，将当前位置压入栈作为新起点
                     stack.push(i);
                 } else {
                     ans = Math.max(ans, i - stack.peek());
