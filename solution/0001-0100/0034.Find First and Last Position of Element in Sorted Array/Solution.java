9ef2d81888eaf408ad2c6b09c6e097e74a36cc4b
laravelshao <laravelshao@163.com>
2024-10-14 15:02:39 +0800

    fix 34. Find First and Last Position of Element in Sorted Array

--- solution/0001-0100/0034.Find First and Last Position of Element in Sorted Array/Solution.java
+++ solution/0001-0100/0034.Find First and Last Position of Element in Sorted Array/Solution.java
@@ -13,13 +13,13 @@ class Solution {
     }
 
     private int search(int[] nums, int x) {
-        int left = 0, right = nums.length;
-        while (left < right) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] < x) {
                 left = mid + 1;
             } else {
-                right = mid;
+                right = mid - 1;
             }
         }
 
