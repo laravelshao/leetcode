f3a5af91ce0d4ddc63e930b30bdf21b43d15b603
laravelshao <laravelshao@163.com>
2024-11-05 20:17:38 +0800

    fix 3. Longest Substring Without Repeating Characters

--- solution/0001-0100/0003.Longest Substring Without Repeating Characters/Solution.java
+++ solution/0001-0100/0003.Longest Substring Without Repeating Characters/Solution.java
@@ -2,18 +2,18 @@ class Solution {
     public int lengthOfLongestSubstring(String s) {
 
         // 记录字符最后一次出现下标位置
-        Map<Character, Integer> dic = new HashMap<>();
+        Map<Character, Integer> charIdxMap = new HashMap<>();
 
         // 定义双指针，i/j 表示不重复子串窗口的开始结束位置
         int i = -1, ans = 0;
         for (int j = 0; j < s.length(); j++) {
-
+            char c = s.charAt(j);
             // 存在重复字符则更新左指针i
-            if (dic.containsKey(s.charAt(j))) {
-                i = Math.max(i, dic.get(s.charAt(j)));
+            if (charIdxMap.containsKey(c)) {
+                i = Math.max(i, charIdxMap.get(c));
             }
             // 更新右指针
-            dic.put(s.charAt(j), j);
+            charIdxMap.put(c, j);
             ans = Math.max(ans, j - i);
         }
 
