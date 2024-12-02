fa36ca2b73bc9490cf246708b6e015ba88f69e2c
laravelshao <laravelshao@163.com>
2024-12-02 21:29:23 +0800

    fix 73. Set Matrix Zeroes

--- solution/0001-0100/0073.Set Matrix Zeroes/Solution.java
+++ solution/0001-0100/0073.Set Matrix Zeroes/Solution.java
@@ -1,28 +1,54 @@
 /**
- * 数组标记法
+ * 原地标记法
  */
-class Solution {
+class Solution2 {
+
     public void setZeroes(int[][] matrix) {
         int row = matrix.length;
         int column = matrix[0].length;
-        // 行零值下标列表
-        List<Integer> rowZeroIndexList = new ArrayList<>();
-        // 列零值下标列表
-        List<Integer> columnZeroIndexList = new ArrayList<>();
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < column; j++) {
-                if (matrix[i][j] == 0) {
-                    rowZeroIndexList.add(i);
-                    columnZeroIndexList.add(j);
-                }
+        // 利用第一行、第一列承载，判断第一行第一列是否需要置零
+        boolean rowZero = false, columnZero = false;
+        for (int j = 0; j < column; j++) {
+            if (matrix[0][j] == 0) {
+                rowZero = true;
+                break;
             }
         }
 
         for (int i = 0; i < row; i++) {
-            for (int j = 0; j < column; j++) {
-                if (rowZeroIndexList.contains(i) || columnZeroIndexList.contains(j)) {
+            if (matrix[i][0] == 0) {
+                columnZero = true;
+                break;
+            }
+        }
+
+        // 从第二行第二列开始遍历
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < column; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < column; j++) {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0) {
                     matrix[i][j] = 0;
                 }
+            }
+        }
+
+        if (rowZero) {
+            for (int j = 0; j < column; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if (columnZero) {
+            for (int i = 0; i < row; i++) {
+                matrix[i][0] = 0;
             }
         }
     }
--- solution/0001-0100/0073.Set Matrix Zeroes/Solution2.java
+++ solution/0001-0100/0073.Set Matrix Zeroes/Solution2.java
@@ -1,54 +1,28 @@
 /**
- * 原地标记法
+ * 数组标记法
  */
-class Solution2 {
-
+class Solution {
     public void setZeroes(int[][] matrix) {
         int row = matrix.length;
         int column = matrix[0].length;
-        // 利用第一行、第一列承载，判断第一行第一列是否需要置零
-        boolean rowZero = false, columnZero = false;
-        for (int j = 0; j < column; j++) {
-            if (matrix[0][j] == 0) {
-                rowZero = true;
-                break;
+        // 行零值下标列表
+        List<Integer> rowZeroIndexList = new ArrayList<>();
+        // 列零值下标列表
+        List<Integer> columnZeroIndexList = new ArrayList<>();
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                if (matrix[i][j] == 0) {
+                    rowZeroIndexList.add(i);
+                    columnZeroIndexList.add(j);
+                }
             }
         }
 
         for (int i = 0; i < row; i++) {
-            if (matrix[i][0] == 0) {
-                columnZero = true;
-                break;
-            }
-        }
-
-        // 从第二行第二列开始遍历
-        for (int i = 1; i < row; i++) {
-            for (int j = 1; j < column; j++) {
-                if (matrix[i][j] == 0) {
-                    matrix[0][j] = 0;
-                    matrix[i][0] = 0;
-                }
-            }
-        }
-
-        for (int i = 1; i < row; i++) {
-            for (int j = 1; j < column; j++) {
-                if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+            for (int j = 0; j < column; j++) {
+                if (rowZeroIndexList.contains(i) || columnZeroIndexList.contains(j)) {
                     matrix[i][j] = 0;
                 }
-            }
-        }
-
-        if (rowZero) {
-            for (int j = 0; j < column; j++) {
-                matrix[0][j] = 0;
-            }
-        }
-
-        if (columnZero) {
-            for (int i = 0; i < row; i++) {
-                matrix[i][0] = 0;
             }
         }
     }
