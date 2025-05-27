c9d819473124a9f0703e09f02b47587579c2de95
laravelshao <laravelshao@163.com>
2025-05-27 20:37:47 +0800

    fix 45. Jump Game II

--- solution/0001-0100/0045.Jump Game II/Solution.java
+++ solution/0001-0100/0045.Jump Game II/Solution.java
@@ -1,6 +1,6 @@
 class Solution {
     public int jump(int[] nums) {
-        // steps：跳跃次数 end：下一次跳跃覆盖的最远距离 maxPosition：最大距离
+        // steps：跳跃次数 end：下一次跳跃覆盖的最远距离 maxDistance：最大距离
         int steps = 0, end = 0, maxDistance = 0;
         // 不需要遍历到最后一个元素，因为我们在遍历最后一个元素前，已经能够覆盖到最后位置，无需额外增加跳跃次数
         for (int i = 0; i < nums.length - 1; i++) {
